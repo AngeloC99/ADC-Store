8ac8150fe3191005b3dec0c883cb8381f30d3308
Chiara-R <chiara.romano1@student.univaq.it>
2021-05-22 15:24:35 +0200

    Prima bozza classe FPersistentManager

---
+++ Foundation/FPersistentManager.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class FPersistentManager
+{
+/** Metodo che permette di salvare un oggetto sul db
+ */
+    public static function store(object $obj) : bool {
+        $Eclass = get_class($obj);
+        $Fclass = str_replace("E", "F", $Eclass);
+        $ris=$Fclass::store($obj);
+        return $ris;
+    }
+
+    /** Metodo che permette di aggiornare un oggetto nel db
+     */
+    public static function update(object $obj) : bool {
+        $Eclass = get_class($obj);
+        $Fclass = str_replace("E", "F", $Eclass);
+        $ris=$Fclass::update($obj);
+        return $ris;
+    }
+
+    public static function load(string $key,string $Fclass) : object{
+        $object=$Fclass::load($key);
+        return $object;
+    }
+
+    public static function exist(string $key,string $Fclass) : bool {
+        $ris = $Fclass::exist($key);
+        return $ris;
+    }
+
+
+    public static function delete(string $key,string $Fclass) : bool {
+        $ris = $Fclass::delete($key);
+        return $ris;
+    }
+}
+
+
+
