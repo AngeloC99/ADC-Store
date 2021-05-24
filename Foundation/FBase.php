751309b5876bab163b49dd54111096f6ecc5982a
Chiara-R <chiara.romano1@student.univaq.it>
2021-05-24 10:20:50 +0200

    Rimozione tipo object

--- Foundation/FBase.php
+++ Foundation/FBase.php
@@ -1,50 +1,16 @@
 <?php
 
-/**
- * Interfaccia che deve essere implementata dalle classi Foundation e che possiede i metodi CRUD ad esse associati.
- * Interface FBase
- * @access public
- * @package Foundation
- */
+
 interface FBase
 {
-    /**
-     * Verifica la presenza di una certa n-upla della tabella, selezionata tramite il valore della chiave passata
-     * come argomento. Ritorna un valore booleano che indica l'esito dell'operazione.
-     * @param string $key
-     * @return bool
-     */
-    public static function exist(string $key): bool;
+    public static function exist(string $key);
 
-    /**
-     * Cancella tutte le n-uple di una tabella nel database aventi per chiave primaria il valore passato come
-     * argomento. Ritorna un valore booleano che indica l'esito dell'operazione.
-     * @param string $key
-     * @return bool
-     */
-    public static function delete(string $key): bool;
+    public static function delete(string $key);
 
-    /**
-     * Restituisce tutte le n-uple di una tabella nel database aventi per chiave primaria il valore passato come
-     * argomento.
-     * @param string $key
-     * @return object
-     */
-    public static function load(string $key): object;
+    public static function load(string $key);
 
-    /**
-     * Memorizza in una tabella del database le informazioni riguardanti l'oggetto passato come argomento.
-     * Ritorna un valore booleano che indica l'esito dell'operazione.
-     * @param object $o
-     * @return bool
-     */
-    public static function store(object $o): bool;
+    public static function store($obj);
 
-    /**
-     * Aggiorna le informazioni inerenti ad un oggetto nel database con il nuovo oggetto passato come argomento.
-     * Ritorna un valore booleano che indica l'esito dell'operazione.
-     * @param object $o
-     * @return bool
-     */
-    public static function update(object $o): bool;
+    public static function update($obj);
+
 }
