7deb829c897fb27a157f477383b1cc50ff9b96de
Chiara-R <chiara.romano1@student.univaq.it>
2021-05-25 08:27:47 +0200

    Create FBuonoSconto.php
    Nuova classe

---
+++ Foundation/FBuonoSconto.php
@@ -0,0 +1,56 @@
+<?php
+
+
+class FBuonoSconto
+{
+    public static function exist(string $key): bool
+    {
+        $pdo=FConnectionDB::connect();
+        $stmt=$pdo->prepare("SELECT * FROM Immagine WHERE codice=?");
+        $ris=$stmt->execute([$key]);
+        return $ris;
+
+    }
+
+    public static function delete(string $key): bool
+    {
+        $pdo=FConnectionDB::connect();
+        $stmt=$pdo->prepare("DELETE FROM Immagine WHERE codice=?");
+        $ris=$stmt->execute([$key]);
+        return $ris;
+    }
+
+    public static function load(string $key) : EBuonoSconto
+    {
+        $pdo=FConnectionDB::connect();
+        $stmt=$pdo->prepare("SELECT * FROM BuonoSconto WHERE codice=?");
+        $stmt->execute([$key]);
+        $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+        $a=$rows['ammontare'];
+        $s=$rows['scadenza'];
+        //$e=$rows['mailutente'];  //l'email serve per recuperare un buono ????????? perchè per istanziarlo non serve
+        $p=$rows['percentuale']; //nuovo campo nella tabella BuonoSconto
+        $bs=new EBuonoSconto($p,$a);
+        $bs->setScadenza($s);
+        return $bs;
+    }
+
+    public static function store($bs): bool
+    {
+        $pdo=FConnectionDB::connect();
+        $query="INSERT INTO BuonoSconto VALUES(?,?,?,?,?)";
+        $stmt=$pdo->prepare($query);
+        $ris=$stmt->execute([$bs->getCodice(),$bs->getAmmontare(),$bs->isPercentuale(),$bs->getScadenza(),$EMAIL??????]);
+        return $ris;
+
+    }
+
+    public static function update($bs1,$bs2=null): bool //parametro di FBase da discutere
+    {
+        $pdo=FConnectionDB::connect();
+        $stmt1 = $pdo->prepare("UPDATE BuonoSconto SET ammontare = $bs1->getAmmontare(), percentuale = $bs1->isPercentuale(), scadenza = $bs1->getScadenza(), mailutente = ?????? WHERE codice=?");
+        $ris1 = $stmt1->execute([$bs1->getCodice()]);
+        return $ris1;
+    }
+
+}
