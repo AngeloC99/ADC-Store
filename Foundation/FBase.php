45344fffc2f0775e78dbe6c892363a17ac1d33ae
AngeloC99 <angelocasciani@live.it>
2021-05-26 15:07:00 +0200

    Aggiornata la definizione dell'interfaccia FBase

--- Foundation/FBase.php
+++ Foundation/FBase.php
@@ -3,7 +3,6 @@
 /**
  * Interfaccia che deve essere implementata dalle classi Foundation e che possiede i metodi CRUD ad esse associati.
  * Interface FBase
- * @access public
  * @package Foundation
  */
 interface FBase
@@ -11,38 +10,48 @@ interface FBase
     /**
      * Verifica la presenza di una certa n-upla della tabella, selezionata tramite il valore della chiave passata
      * come argomento. Ritorna un valore booleano che indica l'esito dell'operazione.
-     * @param string $key
+     * @param $key1
+     * @param $key2
+     * @param $key3
      * @return bool
      */
-    public static function exist(string $key1, string $key2='', string $key3=''): bool;
+    public static function exist($key1, $key2, $key3): bool;
+
 
     /**
      * Cancella tutte le n-uple di una tabella nel database aventi come chiave primaria il valore passato come
-     * argomento. Ritorna un valore booleano che indica l'esito dell'operazione
-     * @param string $key
+     * argomento. Ritorna un valore booleano che indica l'esito dell'operazione.
+     * @param $key1
+     * @param $key2
+     * @param $key3
      * @return bool
      */
-    public static function delete(string $key1, string $key2='', string $key3=''): bool;
+    public static function delete($key1, $key2, $key3): bool;
+
 
     /**
      * Restituisce tutte le n-uple di una tabella nel database aventi per chiave primaria il valore passato come
      * argomento.
-     * @param string $key
-     * @return object
+     * @param $key1
+     * @param $key2
+     * @param $key3
+     * @return mixed
      */
-    public static function load(string $key1, string $key2='', string $key3='');
+    public static function load($key1, $key2, $key3);
 
     /**
      * Memorizza in una tabella del database le informazioni riguardanti l'oggetto passato come argomento.
-     * Ritorna un valore booleano che indica l'esito dell'operazione
-     * @param object $obj
+     * Ritorna un valore booleano che indica l'esito dell'operazione.
+     * @param $obj
      * @return bool
      */
     public static function store($obj): bool;
 
 
     /**
-     * @param $obj1
+     * Aggiorna le informazioni inerenti ad un oggetto salvato nel database e restituisce un booleano che indica
+     * l'esito dell'operazione.
+     * @param $obj
      * @return bool
      */
     public static function update($obj): bool;
