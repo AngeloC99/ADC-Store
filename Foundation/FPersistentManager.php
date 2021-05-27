cb3a95c6f4fb12f1cc675abb73e89301f090df67
Chiara-R <chiara.romano1@student.univaq.it>
2021-05-27 20:55:52 +0200

    Modifiche parametri

--- Foundation/FPersistentManager.php
+++ Foundation/FPersistentManager.php
@@ -3,37 +3,41 @@
 
 class FPersistentManager
 {
-/** Metodo che permette di salvare un oggetto sul db
+    public function __construct()
+    {
+    }
+
+    /** Metodo che permette di salvare un oggetto sul db
  */
-    public static function store(object $obj) : bool {
+    public static function store(object $obj,$mailutente=null) : bool {
         $Eclass = get_class($obj);
         $Fclass = str_replace("E", "F", $Eclass);
-        $ris=$Fclass::store($obj);
+        $ris=$Fclass::store($obj,$mailutente);
         return $ris;
     }
 
     /** Metodo che permette di aggiornare un oggetto nel db
      */
-    public static function update(object $obj1, object $obj2) : bool {
-        $Eclass = get_class($obj1);
+    public static function update(object $obj) : bool {
+        $Eclass = get_class($obj);
         $Fclass = str_replace("E", "F", $Eclass);
-        $ris=$Fclass::update($obj1, $obj2);
+        $ris=$Fclass::update($obj);
         return $ris;
     }
 
-    public static function load(string $key,string $Fclass) : object{
-        $object=$Fclass::load($key);
+    public static function load(string $Fclass,$key1,$key2=null,$key3=null) : object{
+        $object=$Fclass::load($key1,$key2,$key3);
         return $object;
     }
 
-    public static function exist(string $key,string $Fclass) : bool {
-        $ris = $Fclass::exist($key);
+    public static function exist(string $Fclass,$key1,$key2=null,$key3=null) : bool {
+        $ris = $Fclass::exist($key1,$key2,$key3);
         return $ris;
     }
 
 
-    public static function delete(string $key,string $Fclass) : bool {
-        $ris = $Fclass::delete($key);
+    public static function delete(string $Fclass,$key1,$key2=null,$key3=null) : bool {
+        $ris = $Fclass::delete($key1,$key2,$key3);
         return $ris;
     }
 }
