94b8faff112058d13254e5622cdcc5de66067425
Chiara-R <chiara.romano1@student.univaq.it>
2021-06-21 10:42:15 +0200

    Testing view e controller per invio buono

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 require_once "autoloader.php";
 require_once "configDB.php";
-require_once("StartSmarty.php");
 
-/*
-$v = new VHome();
-$v->setTemplate('home.tpl');
+
+//$v = new VHome();
+//$v->setTemplate('home.tpl');
+
+// Testing VBuonoSconto
 $v=new VGestioneBuoni();
-//$v->mostraCreazioneBuono();
-
-$bs=new EBuonoSconto(true,10,"ciao ciao");
-$v->datiBsEmail($bs);
-*/
-
-CGestioneCarrello::recuperaCarrello("Car60c065edcc2f7");
+//$bs=new EBuonoSconto(true,10,"ciao ciao");
+//$v->datiBsEmail($bs);
+$v->mostraCreazioneBuono();
+//CGestioneCarrello::recuperaCarrello("Car60c065edcc2f7");
 
