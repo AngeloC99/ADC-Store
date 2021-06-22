daf5cbf81e4507621dd1b6e61f59744a85f7126b
Chiara-R <chiara.romano1@student.univaq.it>
2021-06-22 16:57:45 +0200

    altro testing

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 require_once "autoloader.php";
 require_once "configDB.php";
-
+require('StartSmarty.php');
 
 //$v = new VHome();
 //$v->setTemplate('home.tpl');
-
 // Testing VBuonoSconto
-$v=new VGestioneBuoni();
+//$v=new VGestioneBuoni();
 //$bs=new EBuonoSconto(true,10,"ciao ciao");
 //$v->datiBsEmail($bs);
-$v->mostraCreazioneBuono();
+//$v->mostraCreazioneBuono();
 //CGestioneCarrello::recuperaCarrello("Car60c065edcc2f7");
 
+?>
