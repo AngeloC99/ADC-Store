c7d19f14cd30c8be74e941c43568bac85a0629b8
AngeloC99 <angelocasciani@live.it>
2021-06-26 16:27:37 +0200

    Gestione login utente

--- View/VGestioneUtenti.php
+++ View/VGestioneUtenti.php
@@ -1,21 +1,37 @@
 <?php
-require_once 'StartSmarty.php';
 
-class VGestionePunti
-{
+
+class VGestioneUtenti {
     private $smarty;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->smarty=StartSmarty::configuration();
     }
 
     public function mostraProfilo($utente){
-
         $this->smarty->assign("path", $GLOBALS["path"]);
         $this->smarty->assign('nome', $utente->getNome());
         $this->smarty->assign('cognome', $utente->getCogome());
         $this->smarty->assign('email', $utente->getEmail());
         $this->smarty->assign('punti', $utente->getPunti());
-        $this->smarty->display('profilefe.tpl');                        
+        $this->smarty->display('profilefe.tpl');
     }
+
+    public function mostraLogin() {
+        $this->smarty->assign("path", $GLOBALS["path"]);
+        $this->smarty->display('loginfe.tpl');
+    }
+
+    public function mostraHomeUtente() {
+        $gs = CGestioneSessioni::getInstance();
+        $this->smarty->assign("utente", $gs->caricaUtente()->getEmail());
+        $this->smarty->assign("path", $GLOBALS["path"]);
+        $this->smarty->display('homeUtente.tpl');
+    }
+
+    public function mostraHomeAdmin() {
+        $this->smarty->assign("path", $GLOBALS["path"]);
+        $this->smarty->display('homeAdmin.tpl');
+    }
+
+}
