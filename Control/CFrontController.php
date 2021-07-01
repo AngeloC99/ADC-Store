28ffc71b584ab65561b523369b9680fc7ee7b54d
Chiara-R <chiara.romano1@student.univaq.it>
2021-07-01 19:46:47 +0200

    Front Controller fine!!!

--- Control/CFrontController.php
+++ Control/CFrontController.php
@@ -7,26 +7,29 @@
  */
 class CFrontController
 {
-
     /**
      * Metodo che viene richiamato ogni volta che viene effettuata una richiesta all'interno del sito. Si occupa di richiamare i vari controllori necessari per l'esecuzione della richiesta effettuata (con eventuali parametri).
      * @param string $path
      */
     public static function run(string $path)
     {
-
         ini_set('session.gc_probability', 10);
         ini_set('session.gc_maxlifetime', 3600);
         $method = $_SERVER['REQUEST_METHOD'];
-
         error_reporting(E_ERROR | E_PARSE);
 
+
+
         $gs = CGestioneSessioni::getInstance();
+
+
 
         if ($gs->isLoggedUser() || $gs->isLoggedAdmin()) {
             if ($path === "/ADC-Store/" || $path === "/ADC-Store/index.php") {
                 setcookie("cookie_test", "cookie_value", time() + 3600);
                 CGestioneSchermate::recuperaHome();
+
+
 
             } else {
                 error_reporting(E_ERROR | E_PARSE);
@@ -36,25 +39,39 @@ class CFrontController
                     $cookie = false;
                 }
 
+
+
                 if ($cookie == true) {
+
+
 
                     $gs = CGestioneSessioni::getInstance();
                     $res = explode("/", $path);
 
+
+
                     array_shift($res);
                     array_shift($res);
                     array_shift($res);
 
+
+
                     if ($res[0] != '') {
+
+
 
                         $controller = "C" . $res[0];
                         $dir = 'Control';
                         $eledir = scandir($dir);
 
+
+
                         if (in_array($controller . ".php", $eledir)) {
                             if (isset($res[1])) {
                                 $function = $res[1];
                                 if (method_exists($controller, $function)) {
+
+
 
                                     $param = array();
                                     for ($i = 2; $i < count($res); $i++) {
@@ -75,11 +92,15 @@ class CFrontController
                 } else {
                     CGestioneSchermate::showCookie();
 
+
+
                 }
             }
         } else {
 
-            if ($path === "/~david/ADC-Store/" || $path === "/ADC-Store/" || $path === "/ADC-Store/index.php" || $path === "/~david/ADC-Store/BackHome") {
+
+
+            if ($path === "/~rommy/ADC-Store/" || $path === "/ADC-Store/" || $path === "/ADC-Store/index.php" || $path === "/~rommy/ADC-Store/BackHome") {
                 setcookie("cookie_test", "cookie_value", time() + 3600);
                 CGestioneSchermate::showHome();
             } else {
@@ -90,25 +111,39 @@ class CFrontController
                     $cookie = false;
                 }
 
+
+
                 if ($cookie == true) {
+
+
 
                     $gs = CGestioneSessioni::getInstance();
                     $res = explode("/", $path);
 
+
+
                     array_shift($res);
                     array_shift($res);
                     array_shift($res);
 
+
+
                     if ($res[0] != '') {
+
+
 
                         $controller = "C" . $res[0];
                         $dir = 'Control';
                         $eledir = scandir($dir);
 
+
+
                         if (in_array($controller . ".php", $eledir)) {
                             if (isset($res[1])) {
                                 $function = $res[1];
                                 if (method_exists($controller, $function)) {
+
+
 
                                     $param = array();
                                     for ($i = 2; $i < count($res); $i++) {
@@ -122,6 +157,8 @@ class CFrontController
                             }
                         }
 
+
+
                     } else {
                         $controller = "CGestioneSchermate";
                         $function = "showHome";
@@ -129,6 +166,8 @@ class CFrontController
                     }
                 } else {
                     CGestioneSchermate::showCookie();
+
+
 
                 }
             }
