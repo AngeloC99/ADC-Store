02d830c76b221af334bf9c3a0fde91349b8d6594
Chiara-R <chiara.romano1@student.univaq.it>
2021-07-01 09:42:09 +0200

    rimozione toString

--- Entity/ECartaCredito.php
+++ Entity/ECartaCredito.php
@@ -130,12 +130,4 @@ class ECartaCredito
         $this->ammontare = $ammontare;
     }
 
-    /**
-     * Implementazione del __toString() per ECartaCredito.
-     * @return string
-     */
-    public function __toString(): string {
-        return $this->circuito." - ".$this->titolare.", "."numero: ".substr($this->numero, 0, 4)."************";
-    }
-
 }
