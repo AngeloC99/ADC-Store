c0ce069036569515c6684bc35e38b130cff17ea3
david99686 <davidmarcus35@gmail.com>
2021-07-01 14:20:36 +0200

    piccolo fix cfrontcontroller

--- Control/CFrontController.php
+++ Control/CFrontController.php
@@ -19,10 +19,10 @@ class CFrontController
         $method = $_SERVER['REQUEST_METHOD'];
 
         error_reporting(E_ERROR | E_PARSE);
-        
+
         $gs = CGestioneSessioni::getInstance();
 
-        if($gs->isLoggedUser() || $gs->isLoggedAdmin()) {
+        if ($gs->isLoggedUser() || $gs->isLoggedAdmin()) {
             if ($path === "/~david/ADC-Store/" || $path === "/ADC-Store/" || $path === "/ADC-Store/index.php") {
                 setcookie("cookie_test", "cookie_value", time() + 3600);
                 CGestioneSchermate::recuperaHome();
@@ -71,68 +71,64 @@ class CFrontController
                         $function = "recuperaHome";
                         $controller::$function();
                     }
-                }
-                else {
+                } else {
                     CGestioneSchermate::showCookie();
 
                 }
             }
-        }
-
-
-        if ($path === "/~david/ADC-Store/" || $path === "/ADC-Store/" || $path === "/ADC-Store/index.php" || $path === "/~david/ADC-Store/BackHome") {
-            setcookie("cookie_test", "cookie_value", time() + 3600);
-            CGestioneSchermate::showHome();
         } else {
-            error_reporting(E_ERROR | E_PARSE);
-            if ($_COOKIE["cookie_test"] == "cookie_value" || isset($_SESSION["cookie_test"]) ) {
-                $cookie = true;
+            if ($path === "/~david/ADC-Store/" || $path === "/ADC-Store/" || $path === "/ADC-Store/index.php" || $path === "/~david/ADC-Store/BackHome") {
+                setcookie("cookie_test", "cookie_value", time() + 3600);
+                CGestioneSchermate::showHome();
             } else {
-                $cookie = false;
-            }
+                error_reporting(E_ERROR | E_PARSE);
+                if ($_COOKIE["cookie_test"] == "cookie_value" || isset($_SESSION["cookie_test"])) {
+                    $cookie = true;
+                } else {
+                    $cookie = false;
+                }
 
-            if ($cookie == true) {
+                if ($cookie == true) {
 
-                $gs = CGestioneSessioni::getInstance();
-                $res = explode("/", $path);
+                    $gs = CGestioneSessioni::getInstance();
+                    $res = explode("/", $path);
 
-                array_shift($res);
-                array_shift($res);
-                array_shift($res);
+                    array_shift($res);
+                    array_shift($res);
+                    array_shift($res);
 
-                if ($res[0] != '') {
+                    if ($res[0] != '') {
 
-                    $controller = "C" . $res[0];
-                    $dir = 'Control';
-                    $eledir = scandir($dir);
+                        $controller = "C" . $res[0];
+                        $dir = 'Control';
+                        $eledir = scandir($dir);
 
-                    if (in_array($controller . ".php", $eledir)) {
-                        if (isset($res[1])) {
-                            $function = $res[1];
-                            if (method_exists($controller, $function)) {
+                        if (in_array($controller . ".php", $eledir)) {
+                            if (isset($res[1])) {
+                                $function = $res[1];
+                                if (method_exists($controller, $function)) {
 
-                                $param = array();
-                                for ($i = 2; $i < count($res); $i++) {
-                                    $param[] = $res[$i];
+                                    $param = array();
+                                    for ($i = 2; $i < count($res); $i++) {
+                                        $param[] = $res[$i];
+                                    }
+                                    $num = count($param);
+                                    if ($num == 0) $controller::$function();
+                                    else if ($num == 1) $controller::$function($param[0]);
+                                    else if ($num == 2) $controller::$function($param[0], $param[1]);
                                 }
-                                $num = count($param);
-                                if ($num == 0) $controller::$function();
-                                else if ($num == 1) $controller::$function($param[0]);
-                                else if ($num == 2) $controller::$function($param[0], $param[1]);
                             }
                         }
+
+                    } else {
+                        $controller = "CGestioneSchermate";
+                        $function = "showHome";
+                        $controller::$function();
                     }
-
                 } else {
-                    $controller = "CGestioneSchermate";
-                    $function = "showHome";
-                    $controller::$function();
+                    CGestioneSchermate::showCookie();
+
                 }
-            }
-
-            else {
-                CGestioneSchermate::showCookie();
-
             }
         }
     }
